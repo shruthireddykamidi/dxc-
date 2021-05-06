6566d8d0f3837a00183042a131d4b35e1b0e933a
shruthi kamidi <kamidishruthireddy@gmail.com>
2021-05-06 22:45:47 +0530

    soap

---
+++ soap/PeopleImpl.java
@@ -0,0 +1,29 @@
+package com.org.p;
+
+
+import com.org.p.*;
+
+public class PeopleImpl implements People {
+	private String name=null;
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+		this.name=name;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	@Override
+	public int geetAge() {
+		// TODO Auto-generated method stub
+		return 20;
+	}
+
+	
+
+}
